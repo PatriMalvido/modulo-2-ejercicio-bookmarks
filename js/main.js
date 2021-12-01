4b6a94c73690a525f48181540524d8385f6bf68d
Patri <patri@malvidopazo.es>
2021-12-01 11:52:33 +0100

    add changes

--- js/main.js
+++ js/main.js
@@ -16,78 +16,80 @@ sectionMenu.classList.remove("collapsed");
     3.3 Añadir esa info con el innerHTML por cada uno de los enlaces
 */
 const listLinks = document.querySelector(".js_datalist");
-const link1 = `<li class="data__listitem"> 
+
+
+/*Dia 2*/
+
+const bkm_1_url ="https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion";
+const bkm_1_desc= "JS en los materiales de Adalab";
+const bkm_1_seen = "checked";
+const bkm_1_tags_1 = "javascript";
+const bkm_1_tags_2 = "html";
+
+
+let html = `<li class="data__listitem"> 
 <article class="data__item">
   <p class="item__url">
-    <a href="https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion" target="_blank" rel="noopener noreferrer">
+    <a href=${bkm_1_url} target="_blank" rel="noopener noreferrer">
       books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion
     </a>
   </p>
   <p class="item__seen">
-    <input type="checkbox" checked name="item_imp_2" id="item_imp_2">
+    <input type="checkbox" ${bkm_1_seen}checked name="item_imp_2" id="item_imp_2">
   </p>
-  <p class="item__desc">JS en los materiales de Adalab</p>
+  <p class="item__desc">${bkm_1_desc}</p>
   <ul class="item__tags">
-    <li class="item__tag">javascript</li><li class="item__tag">HTML</li>
+    <li class="item__tag">${bkm_1_tags_1.toLowerCase()}</li><li class="item__tag">${bkm_1_tags_2.toLowerCase()}</li>
   </ul>
 </article>
 </li>`;
 
-const link2 = ` <li class="data__listitem">
-<article class="data__item">
-  <p class="item__url">
-    <a href="https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/" target="_blank">thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/</a></a></p>
-  <p class="item__seen">
-    <input type="checkbox" checked name="item_imp_1" id="item_imp_1">
-  </p>
-  <p class="item__desc">Ideas de proyectos JS</p>
-  <ul class="item__tags">
-    <li class="item__tag">javascript</li><li class="item__tag">portfolio</li>
-  </ul>
-</article>
-</li>`;
-
-const link3 = `   <li class="data__listitem">
-<article class="data__item">
-  <p class="item__url">
-    <a
-      href="https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web"
-      target="_blank"
-      rel="noopener noreferrer"
-      >books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web</a
-    >
-  </p>
-  <p class="item__seen">
-    <input type="checkbox" name="item_imp_2" id="item_imp_2" />
-  </p>
-  <p class="item__desc">HTML en los materiales de Adalab</p>
-  <ul class="item__tags">
-    <li class="item__tag">HTML</li>
-    <li class="item__tag">CSS</li>
-  </ul>
-</article>
-</li>`;
-
-listLinks.innerHTML = link1 + link2 + link3;
-
-// DIA 2//
-
-const bkm_1_url ="https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion";
 const bkm_2_url = "https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/";
-const bkm_3_url = "https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web";
-
-const bkm_1_desc= "JS en los materiales de Adalab";
 const bkm_2_desc= "Ideas de proyecto JS";
-const bkm_3_desc= "HTML en los materiales de Adalab";
-
-const bkm_1_seen = "checked";
-const bkm_2_seen = "checked";
-const bkm_3_seen = "nochecked";
-
-const bkm_1_tags_1 = "javascript";
-const bkm_1_tags_2 = "html";
 const bkm_2_tags_1 = "javascript";
 const bkm_2_tags_2 = "portfolio";
+
+
+html += ` <li class="data__listitem">
+<article class="data__item">
+  <p class="item__url">
+    <a href=${bkm_2_url}target="_blank">thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/</a></a></p>
+  <p class="item__seen">
+    <input type="checkbox" ${bkm_2_seen}checked name="item_imp_1" id="item_imp_1">
+  </p>
+  <p class="item__desc">${bkm_2_desc}</p>
+  <ul class="item__tags">
+    <li class="item__tag">${bkm_2_tags_1.toLowerCase()}</li><li class="item__tag">${bkm_2_tags_2.toLowerCase()}</li>
+  </ul>
+</article>
+</li>`;
+
+const bkm_3_url = "https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web";
+const bkm_3_desc= "HTML en los materiales de Adalab";
+const bkm_2_seen = "checked";
+const bkm_3_seen = "";
 const bkm_3_tags_1 = "html";
 const bkm_3_tags_2 = "css";
 
+html += `   <li class="data__listitem">
+<article class="data__item">
+  <p class="item__url">
+    <a
+      href=${bmk_3_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      >${bmk_3_url.slice(8)}</a
+    >
+  </p>
+  <p class="item__seen">
+    <input type="checkbox" ${bmk_3_seen} name="item_imp_2" id="item_imp_2" />
+  </p>
+  <p class="item__desc">${bmk_3_desc}</p>
+  <ul class="item__tags">
+    <li class="item__tag">${bmk_3_tags_1.toLowerCase()}</li>
+    <li class="item__tag">${bmk_3_tags_2.toLowerCase()}</li>
+  </ul>
+</article>
+</li>`;
+
+listLinks.innerHTML=html;
